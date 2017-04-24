57e60eeb6c6749054ca668f207b93fb23d676b3d
zhirihuixin <773919412@qq.com>
2017-04-24 17:41:26 +0800

    Update inception.py

--- inception.py
+++ inception.py
@@ -24,3 +24,21 @@ print len(data)
 for i in xrange(len(data)):
     print data[i][0]
     print data[i][1].shape
+
+import h5py
+import cPickle
+import copy
+
+f = h5py.File('D:\wangzhihui\inception\\xception\\xception_weights_tf_dim_ordering_tf_kernels.h5')
+result = []
+a = [[], []]
+def printname(name):
+    print name
+    if ':0' in name:
+        a[0] = name
+        a[1] = f[name][:]
+        result.append(copy.deepcopy(a))
+f.visit(printname)
+
+with open('xception.pkl', 'wb') as f:
+    cPickle.dump(result, f, cPickle.HIGHEST_PROTOCOL)
